8902daf234bbb94cdb18da5413cf5a864bba678b
Francis Pelletier <francispelletier2@hotmail.com>
2023-01-05 14:42:23 -0500

    refactor: createTodoHandler error handling and error message

--- src/controller/todolist.controller.ts
+++ src/controller/todolist.controller.ts
@@ -119,22 +119,27 @@ export async function createTodoHandler(
   const todolist = await findTodolist({ todolistId: todolistId });
 
   if (!todolist) {
-    return res.sendStatus(404);
+    return res.status(404).send("Todolist does not exist");
   }
 
   if (String(todolist.user) !== userId) {
-    return res.sendStatus(403);
+    return res.status(403).send("Unauthorized");
   }
 
-  const updatedTodolist = await findAndCreateTodo(
-    { todolistId: todolistId },
-    update,
-    {
-      new: true,
-    }
-  );
+  try {
+    const updatedTodolist = await findAndCreateTodo(
+      { todolistId: todolistId },
+      update,
+      {
+        new: true,
+      }
+    );
 
-  return res.send(updatedTodolist);
+    return res.send(updatedTodolist);
+  } catch (e: any) {
+    logger.error(e);
+    return res.status(409).send("Something went wrong, please try again");
+  }
 }
 
 export async function deleteTodoHandler(
