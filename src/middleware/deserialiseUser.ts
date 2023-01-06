9cd6801da505d8605b4c6f21a63d0bdbbe061206
Francis Pelletier <francispelletier2@hotmail.com>
2023-01-06 15:19:54 -0500

    feat: deserialiseUser accept cookies for accessToken and refreshToken

--- src/middleware/deserialiseUser.ts
+++ src/middleware/deserialiseUser.ts
@@ -8,12 +8,12 @@ const deserialiseUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = get(req, "headers.authorization", "").replace(
-    /^Bearer\s/,
-    ""
-  );
+  const accessToken =
+    get(req, "cookies.accessToken") ||
+    get(req, "headers.authorization", "").replace(/^Bearer\s/, "");
 
-  const refreshToken = get(req, "headers.x-refresh");
+  const refreshToken =
+    get(req, "cookies.refreshToken") || get(req, "headers.x-refresh");
 
   if (!accessToken) {
     return next();
