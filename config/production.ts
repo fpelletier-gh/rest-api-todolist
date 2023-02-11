7d35931910e18f14149142ce760fcc38d4c7492b
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-10 20:07:34 -0500

    feat: add a config file for production

---
+++ config/production.ts
@@ -0,0 +1,12 @@
+export default {
+  port: process.env.PORT,
+  hostname: process.env.HOSTNAME,
+  dbUri: process.env.DB_URI,
+  saltWorkFactor: 10,
+  accessTokenExpiration: "15m",
+  refreshTokenExpiration: "1y",
+  accessTokenPublicKey: process.env.ACCESS_TOKEN_PUBLIC_KEY,
+  accessTokenPrivateKey: process.env.ACCESS_TOKEN_PRIVATE_KEY,
+  refreshPublicKey: process.env.REFRESH_PUBLIC_KEY,
+  refreshPrivateKey: process.env.REFRESH_PRIVATE_KEY,
+};
