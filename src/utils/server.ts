6e72a271c75793739d073e66dda221e1ead444cc
Francis Pelletier <francispelletier2@hotmail.com>
2023-02-10 20:21:01 -0500

    feat: add cors for development server

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import routes from "../routes";
 import deserializeUser from "../middleware/deserialiseUser";
+import cors from "cors";
 
 function createServer() {
   const app = express();
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:5173",
+    })
+  );
 
   app.use(express.json());
 
